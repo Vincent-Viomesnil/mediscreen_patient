fd90071010bcab36b2a45e412654fc51a0dc35dc
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-04 23:47:46 +0200

    OK Before another branch for the sprint 3

--- src/main/java/com/ocr/mediscreen/exceptions/PatientIntrouvableException.java
+++ src/main/java/com/ocr/mediscreen/exceptions/PatientIntrouvableException.java
@@ -1,8 +1,5 @@
 package com.ocr.mediscreen.exceptions;
 
-import org.springframework.http.HttpStatus;
-
-
 public class PatientIntrouvableException extends RuntimeException {
     public PatientIntrouvableException(String message) {
         super(message);
--- src/main/java/com/ocr/mediscreen/exceptions/PatientNonCreeException.java
+++ src/main/java/com/ocr/mediscreen/exceptions/PatientNonCreeException.java
@@ -1,7 +1,5 @@
 package com.ocr.mediscreen.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class PatientNonCreeException extends RuntimeException {
     public PatientNonCreeException(String message) {
