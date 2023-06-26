948c0e955b9aad5e07d91bfe54a95687ccf650c3
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-26 19:39:11 +0200

    Refacto

--- src/main/java/com/ocr/mediscreen/repository/PatientDAO.java
+++ src/main/java/com/ocr/mediscreen/repository/PatientDAO.java
@@ -1,12 +1,9 @@
 package com.ocr.mediscreen.repository;
 
-import com.ocr.mediscreen.dto.PatientDto;
 import com.ocr.mediscreen.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PatientDAO extends JpaRepository<Patient, Long> {
