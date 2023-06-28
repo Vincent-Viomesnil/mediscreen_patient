13870a420783cf2302ac2e400cbdb00fb1833988
Vincent-Viomesnil <vincent.viomesnil@axa.fr>
2023-06-28 20:17:54 +0200

    Refacto - avant Docker

--- src/main/java/com/ocr/mediscreen/controller/PatientController.java
+++ src/main/java/com/ocr/mediscreen/controller/PatientController.java
@@ -1,17 +1,12 @@
 package com.ocr.mediscreen.controller;
 
 import com.ocr.mediscreen.dto.PatientDto;
-import com.ocr.mediscreen.exceptions.PatientNoCreateException;
 import com.ocr.mediscreen.exceptions.PatientNotFoundException;
-import com.ocr.mediscreen.model.Patient;
 import com.ocr.mediscreen.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class PatientController {
@@ -19,7 +14,7 @@ public class PatientController {
     @Autowired
     private PatientService patientService;
 
-    //Retrieve patients'list
+
     @RequestMapping(value = "/Patients", method = RequestMethod.GET)
     public List<PatientDto> patientList() {
         return patientService.findAll();
--- src/test/java/com/ocr/mediscreen/controller/PatientControllerTest.java
+++ src/test/java/com/ocr/mediscreen/controller/PatientControllerTest.java
@@ -1,22 +1,28 @@
 //package com.ocr.mediscreen.controller;
 //
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import com.ocr.mediscreen.dto.PatientDto;
 //import com.ocr.mediscreen.exceptions.PatientNotFoundException;
-//import com.ocr.mediscreen.model.Patient;
 //import com.ocr.mediscreen.service.PatientService;
 //import org.junit.jupiter.api.BeforeEach;
 //import org.junit.jupiter.api.Test;
 //import org.mockito.InjectMocks;
 //import org.mockito.Mock;
 //import org.mockito.MockitoAnnotations;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 //
 //import java.time.LocalDate;
-//import java.util.ArrayList;
-//import java.util.Date;
+//import java.util.Arrays;
 //import java.util.List;
-//import java.util.Optional;
 //
-//import static org.junit.jupiter.api.Assertions.*;
+//import static org.hamcrest.MatcherAssert.assertThat;
+//import static org.hamcrest.Matchers.*;
 //import static org.mockito.Mockito.*;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 //
 //class PatientControllerTest {
 //
@@ -26,90 +32,68 @@
 //    @InjectMocks
 //    private PatientController patientController;
 //
+//    @Mock
+//    private PatientNotFoundException exceptionHandlerController;
+//
+//    private MockMvc mockMvc;
+//
 //    @BeforeEach
 //    void setUp() {
 //        MockitoAnnotations.openMocks(this);
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.registerModule(new JavaTimeModule());
+//
+//        mockMvc = MockMvcBuilders.standaloneSetup(patientController)
+//                .setControllerAdvice(exceptionHandlerController)
+//                .setHandlerExceptionResolvers()
+//                .setObjectMapper(objectMapper)
+//                .build();
 //    }
 //
 //    @Test
-//    void testPatientList() {
-//        // Mocking the service response
-//        List<Patient> patients = new ArrayList<>();
-//        patients.add(new Patient(1L, "firstname", "firstname",  LocalDate.of(1990,04,01), "F", "address", "0102030405"));
-//        patients.add(new Patient(2L, "lastname", "lastname",  LocalDate.of(1990,04,01), "F", "address", "1234567890"));
+//    void patientList_ReturnsListOfPatients() throws Exception {
+//        // Arrange
+//        PatientDto patient1 = new PatientDto(1L, "Firstname", "Lastname", LocalDate.of(1990, 01, 01), "M", "address", "010203040506");
+//        PatientDto patient2 = new PatientDto(2L, "Prenom", "Nom", LocalDate.of(1990, 01, 01), "F", "postalcode", "1234567890");
+//        List<PatientDto> patients = Arrays.asList(patient1, patient2);
+//
 //        when(patientService.findAll()).thenReturn(patients);
 //
-//        // Calling the controller method
-//        List<Patient> result = patientController.patientList();
+//        // Act & Assert
+//        mockMvc.perform(get("/Patients"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(result -> {
+//                    String contentAsString = result.getResponse().getContentAsString();
+//                    List<PatientDto> responsePatients = Arrays.asList(new ObjectMapper().readValue(contentAsString, PatientDto[].class));
 //
-//        // Verifying the service method was called
+//                    // Assert the number of patients returned
+//                    assertThat(responsePatients.size(), is(2));
+//
+//                    // Assert the properties of the first patient
+//                    PatientDto responsePatient1 = responsePatients.get(0);
+//                    assertThat(responsePatient1.getId(), is(1L));
+//                    assertThat(responsePatient1.getFirstname(), is("Firstname"));
+//                    assertThat(responsePatient1.getLastname(), is("Lastname"));
+//                    assertThat(responsePatient1.getBirthdate(), is(LocalDate.of(1990, 01, 01)));
+//                    assertThat(responsePatient1.getGender(), is("M"));
+//                    assertThat(responsePatient1.getAddress(), is("address"));
+//                    assertThat(responsePatient1.getPhonenumber(), is("010203040506"));
+//
+//                    // Assert the properties of the second patient
+//                    PatientDto responsePatient2 = responsePatients.get(1);
+//                    assertThat(responsePatient2.getId(), is(2L));
+//                    assertThat(responsePatient2.getFirstname(), is("Prenom"));
+//                    assertThat(responsePatient2.getLastname(), is("Nom"));
+//                    assertThat(responsePatient2.getBirthdate(), is(LocalDate.of(1990, 01, 01)));
+//                    assertThat(responsePatient2.getGender(), is("F"));
+//                    assertThat(responsePatient2.getAddress(), is("postalcode"));
+//                    assertThat(responsePatient2.getPhonenumber(), is("1234567890"));
+//                });
+//
 //        verify(patientService, times(1)).findAll();
-//
-//        // Asserting the result
-//        assertEquals(patients, result);
+//        verifyNoMoreInteractions(patientService);
 //    }
 //
-//    @Test
-//    void testGetPatientByLastname() {
-//        // Mocking the service response
-//
-//        Optional<Patient> patient = Optional.of(new Patient(1L, "firstname", "firstname",  LocalDate.of(1990,04,01), "F", "address", "0102030405"));
-//
-//        when(patientService.findByLastname("Doe")).thenReturn(patient);
-//
-//        // Calling the controller method
-//        Optional<Patient> result = patientController.getPatientByLastname("Doe");
-//
-//        // Verifying the service method was called
-//        verify(patientService, times(1)).findByLastname("Doe");
-//
-//        // Asserting the result
-//        assertEquals(patient, result);
-//    }
-//
-//    @Test
-//    void testGetPatientByLastnameNoPatient() {
-//        // Mocking the service response
-//        when(patientService.findByLastname("Lastname")).thenReturn(Optional.empty());
-//
-//        // Calling the controller method
-//        assertThrows(PatientNotFoundException.class, () -> {
-//            patientController.getPatientByLastname("Lastname");
-//        });
-//
-//        // Verifying the service method was called
-//        verify(patientService, times(1)).findByLastname("Lastname");
-//    }
-//
-//    @Test
-//    void testGetPatientById() {
-//        // Mocking the service response
-//        Date birthdate = new Date();
-//        Optional<Patient> patient = Optional.of(new Patient(1L, "firstname", "firstname", LocalDate.of(1990,04,01), "F", "address", "0102030405"));
-//        when(patientService.findById(1L)).thenReturn(patient);
-//
-//        // Calling the controller method
-//        Optional<Patient> result = patientController.getPatientById(1L);
-//
-//        // Verifying the service method was called
-//        verify(patientService, times(1)).findById(1L);
-//
-//        // Asserting the result
-//        assertEquals(patient, result);
-//    }
-//
-//    @Test
-//    void testGetPatientByIdNonExistingPatient() {
-//        // Mocking the service response
-//        when(patientService.findById(1L)).thenReturn(Optional.empty());
-//
-//        // Calling the controller method
-//        assertThrows(PatientNotFoundException.class, () -> {
-//            patientController.getPatientById(1L);
-//        });
-//
-//        // Verifying the service method was called
-//        verify(patientService, times(1)).findById(1L);
-//    }
 //}
-//
